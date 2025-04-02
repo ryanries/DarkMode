d74cb69062f750302a3371353599b5cad17e4a5e
Ryan Ries <ryanries09@gmail.com>
2025-04-02 14:40:46 -0500

    episode 001

---
+++ Episode001/RemoteDllInjector.c
@@ -0,0 +1,236 @@
+// Dark Mode Episode 001
+// https://www.youtube.com/playlist?list=PLlaINRtydtNUwkwdmCBNtkwgVRda8Ya_G
+// Joseph Ryan Ries
+
+// Temporarily disable warnings originating from windows.h, over which we have no control.
+#pragma warning(push, 0)
+
+#pragma warning(disable: 4668)
+
+#include <Windows.h>
+
+// Restore warning level to /Wall
+#pragma warning(pop)
+
+#include <stdio.h>
+
+// Usage: RemoteDllInjector.exe <ProcessID> <PathToDLL>
+// This program writes the DLL name, e.g. "C:\temp\mydll.dll", into the memory of the remote
+// process of your choosing. It then creates a new thread in that remote process, with the start
+// address of the new thread being LoadLibraryW, and the argument being the DLL to load. This
+// causes the remote process to load the arbitrary DLL, which will automatically execute DllMain
+// of the newly-loaded module.
+
+
+// Returns TRUE if the given file exists. Returns FALSE if the file does not exist or is a directory.
+BOOL FileExistsW(wchar_t* FileName)
+{
+	DWORD FileAttributes = GetFileAttributesW(FileName);
+
+	return (FileAttributes != INVALID_FILE_ATTRIBUTES && !(FileAttributes & FILE_ATTRIBUTE_DIRECTORY));
+}
+
+
+int wmain(int argc, wchar_t* argv[], wchar_t* envp[])
+{
+	UNREFERENCED_PARAMETER(envp);
+
+	DWORD ReturnCode = ERROR_SUCCESS;
+
+	wchar_t* UsageString = L"USAGE: RemoteDllInjector.exe <ProcessID> <PathToDLL>\n";
+
+	// The path to the DLL we want to inject. Must actually exist as a file on disk.
+	wchar_t* DllPath = NULL;
+
+	// The process ID (pid) of the process we want to inject into.
+	DWORD ProcessID = 0;
+
+	HANDLE RemoteProcessHandle = INVALID_HANDLE_VALUE;
+
+	HANDLE Kernel32ModuleHandle = INVALID_HANDLE_VALUE;
+
+	void* LoadLibraryAddress = NULL;
+
+	void* RemoteMemoryForDllName = NULL;
+
+	if (argc != 3)
+	{
+		ReturnCode = ERROR_INVALID_PARAMETER;
+
+		wprintf(L"%s", UsageString);
+
+		goto Exit;
+	}
+
+	if ((ProcessID = _wtoi(argv[1])) == 0)
+	{
+		ReturnCode = ERROR_INVALID_PARAMETER;
+
+		wprintf(L"[-] Cannot convert ProcessID!\n");
+
+		wprintf(L"%s", UsageString);
+
+		goto Exit;
+	}
+
+	DllPath = argv[2];
+
+	if (FileExistsW(DllPath) == FALSE)
+	{
+		ReturnCode = ERROR_FILE_NOT_FOUND;
+
+		wprintf(L"[-] Cannot locate DLL!\n");
+
+		wprintf(L"%s", UsageString);
+
+		goto Exit;
+	}
+
+	RemoteProcessHandle = OpenProcess(PROCESS_ALL_ACCESS, FALSE, ProcessID);
+
+	if (RemoteProcessHandle == NULL)
+	{
+		// If OpenProcess failed with access denied, we can try again after enabling debug
+		// privilege. If it failed for some other reason, we need to bail.
+		if (GetLastError() != ERROR_ACCESS_DENIED)
+		{
+			ReturnCode = GetLastError();
+
+			wprintf(L"[-] OpenProcess failed with 0x%08lx!\n", ReturnCode);
+
+			goto Exit;
+		}
+
+		HANDLE CurrentProcessTokenHandle = NULL;
+
+		LUID Luid = { 0 };
+
+		TOKEN_PRIVILEGES TokenPrivileges = { 0 };
+
+		if (OpenProcessToken(GetCurrentProcess(), TOKEN_ADJUST_PRIVILEGES, &CurrentProcessTokenHandle) == 0)
+		{
+			ReturnCode = GetLastError();
+
+			wprintf(L"[-] OpenProcessToken failed with error 0x%08lx!\n", ReturnCode);
+
+			goto Exit;
+		}		
+
+		LookupPrivilegeValueW(NULL, SE_DEBUG_NAME, &Luid);
+
+		TokenPrivileges.PrivilegeCount = 1;
+
+		TokenPrivileges.Privileges[0].Luid = Luid;
+
+		TokenPrivileges.Privileges[0].Attributes = SE_PRIVILEGE_ENABLED;
+
+		if (AdjustTokenPrivileges(
+			CurrentProcessTokenHandle,
+			FALSE,
+			&TokenPrivileges,
+			0,
+			(PTOKEN_PRIVILEGES)NULL,
+			(PDWORD)NULL) == 0)
+		{
+			ReturnCode = GetLastError();
+
+			wprintf(L"[-] AdjustTokenPrivileges failed with error 0x%08lx!\n", ReturnCode);
+
+			goto Exit;
+		}
+
+		wprintf(L"[+] Successfully enabled DEBUG privilege.\n");
+
+		RemoteProcessHandle = OpenProcess(PROCESS_ALL_ACCESS, FALSE, ProcessID);
+
+		if (RemoteProcessHandle == NULL)
+		{
+			ReturnCode = GetLastError();
+
+			wprintf(L"[-] OpenProcess failed with error 0x%08lx\n", ReturnCode);
+
+			goto Exit;
+		}		
+	}
+
+	// Notice the address of kernel32.dll will be the same both in our process and in the remote process.
+	Kernel32ModuleHandle = GetModuleHandleW(L"kernel32.dll");
+
+	if (Kernel32ModuleHandle == NULL)
+	{
+		ReturnCode = GetLastError();
+
+		wprintf(L"[-] GetModuleHandleW failed with error 0x%08lx\n", ReturnCode);
+
+		goto Exit;
+	}
+
+	// Locate the address of the LoadLibrary function within the kernel32.dll module.
+	LoadLibraryAddress = (LPVOID)GetProcAddress(Kernel32ModuleHandle, "LoadLibraryW");
+
+	if (LoadLibraryAddress == NULL)
+	{
+		ReturnCode = GetLastError();
+
+		wprintf(L"[-] GetProcAddress failed with error 0x%08lx\n", ReturnCode);
+
+		goto Exit;
+	}
+
+	// Allocate some memory in the remote process - just enough for us to write a copy of the 
+	// DLL path into the remote processes' memory.
+	RemoteMemoryForDllName = VirtualAllocEx(
+		RemoteProcessHandle,
+		NULL,
+		wcslen(DllPath) * sizeof(wchar_t),
+		MEM_RESERVE | MEM_COMMIT,
+		PAGE_READWRITE);
+
+	if (RemoteMemoryForDllName == NULL)
+	{
+		ReturnCode = GetLastError();
+
+		wprintf(L"[-] VirtualAllocEx failed with error 0x%08lx\n", ReturnCode);
+
+		goto Exit;
+	}
+
+	wprintf(L"[+] Allocated memory in remote process.\n");
+
+	if ((WriteProcessMemory(RemoteProcessHandle, RemoteMemoryForDllName, DllPath, wcslen(DllPath) * sizeof(wchar_t), NULL)) == 0)
+	{
+		ReturnCode = GetLastError();
+
+		wprintf(L"[-] WriteProcessMemory failed with error 0x%08lx\n", ReturnCode);
+
+		goto Exit;
+	}
+
+	// Start a new thread in the remote process, specifying a start location of LoadLibrary.
+	// This is equivalent to the remote process calling LoadLibrary(L"C:\temp\my.dll");
+	// When you load a DLL via LoadLibrary, you also automatically and immediately run whatever
+	// code is contained in that module's DllMain function. That means this is an easy way to 
+	// get a remote process to run whatever arbitrary code we want.
+
+	if (CreateRemoteThread(
+		RemoteProcessHandle,
+		NULL,
+		0,
+		(LPTHREAD_START_ROUTINE)LoadLibraryAddress,
+		RemoteMemoryForDllName,
+		0,
+		NULL) == NULL)
+	{
+		ReturnCode = GetLastError();
+
+		wprintf(L"[-] CreateRemoteThread failed with error 0x%08lx\n", ReturnCode);
+
+		goto Exit;
+	}
+
+	wprintf(L"[+] Successfully injected DLL into process with PID %ld!\n", ProcessID);
+
+Exit:
+
+	return(ReturnCode);
+}
